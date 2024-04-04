20090212743f8f5dbb3eb36db2b6b4429f4d16ca
AngerOverApathy <violasian@gmail.com>
2024-04-03 20:53:02 -0700

    connected api

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [gameTitle, setGameTitle] = useState('')
 
+  const searchGame = () => {
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
 
   return (
     <>
     <div className="App">
       <div className='searchSection'>
         <h1>Search For A Game</h1>
-        <input type='text' placeholder='Search for a game...'/>
-        <button>Search Game Title</button>
+        <input type='text' placeholder='Search for a game...' onChange={(event)=> {
+          setGameTitle(event.target.value)
+        }}/>
+        <button onClick={searchGame}>Search Game Title</button>
       </div>
       
       <div className='dealsSection'>
